50bd4f6d6d149c4f57f9a1ce470bd17e37de6a87
chandrika-kp <102351627+chandrika-kp@users.noreply.github.com>
2023-03-17 22:27:00 +0530

    Update LogginTernary.js

--- src/components/LogginTernary.js
+++ src/components/LogginTernary.js
@@ -1,16 +1,40 @@
 import React from 'react'
 let element = null;
 let isLoggedIn = false;
-  
 
- function LogginTernary() {
-  return (
-    <div>
-      isLoggedIn
+// Using if-else:
+
+// function App() {
+//   let element = null;
+//   let isLoggedIn = false;
+//   if (isLoggedIn) {
+//     element = <h2>Welcome Admin</h2>;
+//   } else {
+//     element = <h2>Please Login</h2>;
+//   }
+//   return <>{element}</>;
+// }
+// export default App;
+
+
+// 2. Using ternary operator:
+
+// function App() {
+//   let isLoggedIn = false;
+//   return isLoggedIn ? <h2>Welcome Admin</h2> : <h2>Please Login</h2>;
+// }
+// export default App;
+
+// 3. Using variables:
+
+function LogginTernary() {
+  isLoggedIn
     ? (element = <h2>Welcome Admin</h2>)
     : (element = <h2>Please Login</h2>);
-      
+  return (
+    <div>
+      {element} 
     </div>
   )
 }
-export default LogginTernary()
+export default LogginTernary
